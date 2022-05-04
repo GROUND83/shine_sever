d8f4ffd60e141090f0a3c29e4686ecd4cdeb9574
ground83 <wiseguy830@naver.com>
2022-05-04 19:41:01 +0900

    de2

--- config/settings.py
+++ config/settings.py
@@ -180,8 +180,8 @@ USE_TZ = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = ""
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 # STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
